e949438b462437695553b86609f3b74a45a0ac35
hf_hasegawLab_pc <vegil@vip.qq.com>
2026-02-05 20:25:15 +0900

    Fix __init__.py.

--- dft_codes/__init__.py
+++ dft_codes/__init__.py
@@ -7,7 +7,6 @@ from .openmx import (
     compute_spectral_function,
 )
 from .wannier90 import MLWFHamiltonian, wannier90_contourmap, load_band_contourmap, _save_band_contourmap, GPUContext
-from .STM import LATTICE2D, QPICalculator, LatticeOperations, create_polygon_mask
 
 __all__ = [
     "OpenMX",
@@ -19,10 +18,6 @@ __all__ = [
     "_save_band_contourmap",
     "GPUContext",
     "load_dos_tree",
-    "LATTICE2D",
-    "QPICalculator",
-    "LatticeOperations",
     "read_unfold_orbup",
     "compute_spectral_function",
-    "create_polygon_mask",
 ]
